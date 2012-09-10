f90255797d9158b9df2d37480f3b72614b80d275
Gregg Caines <gcaines@exacttarget.com>
2012-09-10 16:33:23 -0700

    added signed BE scenarios

--- index.js
+++ index.js
@@ -22,6 +22,25 @@ BufferMaker.prototype.UInt64BE = function(val){
   return this;
 };
 
+BufferMaker.prototype.Int8 = function(val){
+  this.plan.push({ type : "Int8", value : val});
+  return this;
+};
+BufferMaker.prototype.Int16BE = function(val){
+  this.plan.push({ type : "Int16BE", value : val});
+  return this;
+};
+
+BufferMaker.prototype.Int32BE = function(val){
+  this.plan.push({ type : "Int32BE", value : val});
+  return this;
+};
+
+BufferMaker.prototype.Int64BE = function(val){
+  this.plan.push({ type : "Int64BE", value : bignum(val)});
+  return this;
+};
+
 BufferMaker.prototype.string = function(val){
   this.plan.push({ type : "string", value : val});
   return this;
@@ -39,6 +58,10 @@ BufferMaker.prototype.make = function(){
       case "UInt16BE": bytecount += 2; break;
       case "UInt32BE": bytecount += 4; break;
       case "UInt64BE": bytecount += 8; break;
+      case "Int8": bytecount += 1; break;
+      case "Int16BE": bytecount += 2; break;
+      case "Int32BE": bytecount += 4; break;
+      case "Int64BE": bytecount += 8; break;
       case "string": bytecount += item.value.length; break;
     }
   }
@@ -53,6 +76,16 @@ BufferMaker.prototype.make = function(){
         var bit64Buffer = item.value.toBuffer({endian : "big", size : 8});
         for(j = 0; j < bit64Buffer.length; j++){
           buffer[offset + j] = bit64Buffer[j];
+        }
+        offset += 8;
+        break;
+      case "Int8": buffer.writeInt8(item.value, offset); offset += 1; break;
+      case "Int16BE": buffer.writeInt16BE(item.value, offset); offset += 2; break;
+      case "Int32BE": buffer.writeInt32BE(item.value, offset); offset += 4; break;
+      case "Int64BE":
+        var signed64BitBuffer = item.value.toBuffer({endian : "big", size : 8});
+        for(j = 0; j < signed64BitBuffer.length; j++){
+          buffer[offset + j] = signed64BitBuffer[j];
         }
         offset += 8;
         break;
--- test/BufferMaker.js
+++ test/BufferMaker.js
@@ -2,55 +2,99 @@ require('should');
 var BufferMaker = require('../index');
 
 describe("BufferMaker", function(){
-  it("can create a one byte buffer", function(){
-    var buffer = new Buffer(1);
-    buffer.writeUInt8(5,0);
-    var actual = new BufferMaker().UInt8(5).make();
-    actual.should.eql(buffer);
-  });
-  it("can create a two-byte (16-bit) buffer", function(){
-    var buffer = new Buffer(2);
-    buffer.writeUInt16BE(5,0);
-    var actual = new BufferMaker().UInt16BE(5).make();
-    actual.should.eql(buffer);
-  });
-  it("can create a four-byte (32-bit) buffer", function(){
-    var buffer = new Buffer(4);
-    buffer.writeUInt32BE(5,0);
-    var actual = new BufferMaker().UInt32BE(5).make();
-    actual.should.eql(buffer);
-  });
-  it("can create an eight-byte (64-bit) buffer", function(){
-    var buffer = new Buffer(8);
-    buffer.writeUInt32BE(0,0);
-    buffer.writeUInt32BE(5,4);
-    var actual = new BufferMaker().UInt64BE(5).make();
-    actual.should.eql(buffer);
-  });
-  it("can create a buffer from a string", function(){
-    var buffer = new Buffer(8);
-    buffer.write("12345678");
-    var actual = new BufferMaker().string("12345678").make();
-    actual.should.eql(buffer);
-  });
-  it("can create a buffer from a buffer", function(){
-    var buffer = new Buffer(8);
-    buffer.write("12345678");
-    var expected = new BufferMaker().string(buffer).make();
-    var actual = new BufferMaker().string("12345678").make();
-    actual.should.eql(expected);
-  });
+  
+  describe("when used for big endian scenarios", function(){
+
+    describe("when used for unsigned input", function(){
+      it("can create a one byte buffer", function(){
+        var buffer = new Buffer(1);
+        buffer.writeUInt8(5,0);
+        var actual = new BufferMaker().UInt8(5).make();
+        actual.should.eql(buffer);
+      });
+      it("can create a two-byte (16-bit) buffer", function(){
+        var buffer = new Buffer(2);
+        buffer.writeUInt16BE(5,0);
+        var actual = new BufferMaker().UInt16BE(5).make();
+        actual.should.eql(buffer);
+      });
+      it("can create a four-byte (32-bit) buffer", function(){
+        var buffer = new Buffer(4);
+        buffer.writeUInt32BE(5,0);
+        var actual = new BufferMaker().UInt32BE(5).make();
+        actual.should.eql(buffer);
+      });
+      it("can create an eight-byte (64-bit) buffer", function(){
+        var buffer = new Buffer(8);
+        buffer.writeUInt32BE(0,0);
+        buffer.writeUInt32BE(5,4);
+        var actual = new BufferMaker().UInt64BE(5).make();
+        actual.should.eql(buffer);
+      });
+      it ("can create a buffer from a binary string", function(){
+        var messageSetBufferMaker = new BufferMaker();
+        var encodedMessage = new Buffer("four");
+        var messageSet = messageSetBufferMaker
+                      .UInt32BE(encodedMessage.length)
+                      .string(encodedMessage)
+                      .make();
+        messageSet.should.eql(new Buffer([0, 0, 0, 4, 'f'.charCodeAt(0), 'o'.charCodeAt(0), 'u'.charCodeAt(0), 'r'.charCodeAt(0)]));
+      });
+    });
+
+    describe("when used for signed input", function(){
+      it("can create a one byte buffer", function(){
+        var buffer = new Buffer(1);
+        buffer.writeInt8(5,0);
+        var actual = new BufferMaker().Int8(5).make();
+        actual.should.eql(buffer);
+      });
+      it("can create a two-byte (16-bit) buffer", function(){
+        var buffer = new Buffer(2);
+        buffer.writeInt16BE(5,0);
+        var actual = new BufferMaker().Int16BE(5).make();
+        actual.should.eql(buffer);
+      });
+      it("can create a four-byte (32-bit) buffer", function(){
+        var buffer = new Buffer(4);
+        buffer.writeInt32BE(5,0);
+        var actual = new BufferMaker().Int32BE(5).make();
+        actual.should.eql(buffer);
+      });
+      it("can create an eight-byte (64-bit) buffer", function(){
+        var buffer = new Buffer(8);
+        buffer.writeInt32BE(0,0);
+        buffer.writeInt32BE(5,4);
+        var actual = new BufferMaker().Int64BE(5).make();
+        actual.should.eql(buffer);
+      });
+    });
+
+    it ("can create a buffer from a binary string", function(){
+      var messageSetBufferMaker = new BufferMaker();
+      var encodedMessage = new Buffer("four");
+      var messageSet = messageSetBufferMaker
+                    .UInt32BE(encodedMessage.length)
+                    .string(encodedMessage)
+                    .make();
+      messageSet.should.eql(new Buffer([0, 0, 0, 4, 'f'.charCodeAt(0), 'o'.charCodeAt(0), 'u'.charCodeAt(0), 'r'.charCodeAt(0)]));
+    });
+    it("can create a buffer from a string", function(){
+      var buffer = new Buffer(8);
+      buffer.write("12345678");
+      var actual = new BufferMaker().string("12345678").make();
+      actual.should.eql(buffer);
+    });
+
+    it("can create a buffer from a buffer", function(){
+      var buffer = new Buffer(8);
+      buffer.write("12345678");
+      var expected = new BufferMaker().string(buffer).make();
+      var actual = new BufferMaker().string("12345678").make();
+      actual.should.eql(expected);
+    });
 
 
-  it ("can create a buffer from a binary string", function(){
-    var messageSetBufferMaker = new BufferMaker();
-    var encodedMessage = new Buffer("four");
-    var messageSet = messageSetBufferMaker
-                  .UInt32BE(encodedMessage.length)
-                  .string(encodedMessage)
-                  .make();
-    messageSet.should.eql(new Buffer([0, 0, 0, 4, 'f'.charCodeAt(0), 'o'.charCodeAt(0), 'u'.charCodeAt(0), 'r'.charCodeAt(0)]));
   });
-
 
 });
